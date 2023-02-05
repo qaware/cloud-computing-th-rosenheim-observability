deacfc27c5d2f33d436e837c2ee97cdd07ff278f
Franz Wimmer <franz.wimmer@qaware.de>
2023-02-05 03:14:09 +0100

    Switch to Java 17

--- tle-fetcher/build.gradle.kts
+++ tle-fetcher/build.gradle.kts
@@ -44,8 +44,8 @@ group = "de.qaware.cloudcomputing"
 version = "1.0.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<Test> {
