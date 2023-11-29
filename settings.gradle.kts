7b2c7ec55ae5859cb6d55d00828497019dc3b7e8
Franz Wimmer <franz.wimmer@qaware.de>
2023-11-29 18:28:49 +0100

    Add gradle setup

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+    plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
+    }
+}
+rootProject.name="observability-from-scratch"
+include("tle-fetcher")
+include("sky-map")
+
